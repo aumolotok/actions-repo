3808c6b47e57b6553dc53f3e24e57e1ad91820cf
Andrey Kolotilo <aummolotok@gmail.com>
2022-11-23 14:01:25 +0300

    add read env

--- actions/fillRequest/index.ts
+++ actions/fillRequest/index.ts
@@ -1,10 +1,27 @@
 import * as core from "@actions/core"
-// import * as github from "@actions/github"
+import * as github from "@actions/github"
 
 
     async function fill() {
 
         const token = core.getInput("token");
+
+        const credentials = {
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+        };
+
+        const octokit = github.getOctokit(token);
+
+
+
+
+        const fs = require('fs')
+        const ev = JSON.parse(
+        fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8')
+        )
+        console.log(ev)
+        const prNum = ev.pull_request.number
 
         // const info = core.getInput("pullRequestInfo")
 
