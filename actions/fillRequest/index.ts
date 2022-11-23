1bb944f68f282ee43f234897a61dead79c904d6f
Andrey Kolotilo <aummolotok@gmail.com>
2022-11-23 12:35:24 +0300

    add token

--- actions/fillRequest/index.ts
+++ actions/fillRequest/index.ts
@@ -2,11 +2,11 @@ import * as core from "@actions/core"
 import * as github from "@actions/github"
 
     async function fill() {
-        // const token = core.getInput("token");
+        const token = core.getInput("token");
 
         // const info = core.getInput("pullRequestInfo")
 
-        console.log(`Hello to new pull request`)
+        console.log(`Hello to new pull request + ${token}`)
 
     }
 
