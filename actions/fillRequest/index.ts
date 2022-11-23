71105078184914541bd5353172fc88ac80340d51
Andrey Kolotilo <aummolotok@gmail.com>
2022-11-23 12:48:54 +0300

    add requare

--- actions/fillRequest/index.ts
+++ actions/fillRequest/index.ts
@@ -1,12 +1,15 @@
-import * as core from "@actions/core"
-import * as github from "@actions/github"
+// import * as core from "@actions/core"
+// import * as github from "@actions/github"
+    const core = require("@actions/core")
 
     async function fill() {
-        const token = core.getInput("token");
+
+        //const token = core.getInput("token");
 
         // const info = core.getInput("pullRequestInfo")
 
-        console.log(`Hello to new pull request + ${token}`)
+        //console.log(`Hello to new pull request + ${token}`)
+        console.log(`Hello to new pull request`)
 
     }
 
