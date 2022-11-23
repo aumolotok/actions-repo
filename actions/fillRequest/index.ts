e75a07f4568711676b7e985f5a29b75ce2c68e1c
Andrey Kolotilo <aummolotok@gmail.com>
2022-11-23 12:05:03 +0300

    start action commit

---
+++ actions/fillRequest/index.ts
@@ -0,0 +1,13 @@
+// import * as core from "@actions/core"
+// import * as github from "@actions/github"
+
+    async function fill() {
+        // const token = core.getInput("token");
+
+        // const info = core.getInput("pullRequestInfo")
+
+        console.log(`Hello to new pull request`)
+
+    }
+
+    fill()
